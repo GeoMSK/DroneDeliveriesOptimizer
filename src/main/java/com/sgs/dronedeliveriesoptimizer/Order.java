e95702cc939c14d2ed42ee4e7fc0231766b58402
George Mantakos <GeoMSKg@gmail.com>
2017-02-10 16:47:57 +0200

    Order initialization fix

--- src/main/java/com/sgs/dronedeliveriesoptimizer/Order.java
+++ src/main/java/com/sgs/dronedeliveriesoptimizer/Order.java
@@ -12,10 +12,10 @@ public class Order {
     private final Position deliveryPos;
     private final int productsNo;
     private final int[] productTypes;
-    private HashMap<Integer, Integer> productQuantityPerType;
+    private final HashMap<Integer, Integer> productQuantityPerType;
 
     /**
-     * 
+     *
      * @param deliveryPos the position in the grid where the delivery should be made
      * @param productsNo the total number of products in this order
      * @param productTypes the type of each product
@@ -24,24 +24,23 @@ public class Order {
         this.deliveryPos = deliveryPos;
         this.productsNo = productsNo;
         this.productTypes = productTypes;
-        
+        this.productQuantityPerType = new HashMap<>();
+
         for (int i = 0; i < productsNo; i++) {
-           int typeId = productTypes[i];
+            int typeId = productTypes[i];
             Integer quantity = productQuantityPerType.get(typeId);
             if (quantity == null) {
                 productQuantityPerType.put(typeId, 1);
             } else {
                 productQuantityPerType.compute(typeId, (k, v) -> {
-                    return v+1;
+                    return v + 1;
                 });
             }
-           
-            
         }
     }
 
     /**
-     * 
+     *
      * @return the position in the grid where the delivery should be made
      */
     public Position getDeliveryPos() {
@@ -49,7 +48,7 @@ public class Order {
     }
 
     /**
-     * 
+     *
      * @return the total number of products in this order
      */
     public int getProductsNo() {
@@ -57,16 +56,16 @@ public class Order {
     }
 
     /**
-     * 
+     *
      * @return the type of each product
      */
     public int[] getProductTypes() {
         return productTypes;
     }
-    
+
     /**
      * Products arrived for this order, remove them from list
-     * 
+     *
      * @param productTypeId the id of the type of the product
      * @param productNum the number of products to pick up
      */
@@ -83,7 +82,7 @@ public class Order {
             markComplete();
         }
     }
-    
+
     /**
      * This should be called during simulation when the order gets completed
      */
